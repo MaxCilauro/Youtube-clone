3af1ce53509476671d298ea736655f594e9c9f3f
Max Cilauro <maxicilauro@gmail.com>
2020-10-19 17:14:20 +0200

    add base secrets file

---
+++ youtube-clone/Services/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  youtube-clone
+//
+//  Created by Yaku on 06/10/2020.
+//  Copyright © 2020 Uppercaseme. All rights reserved.
+//
+
+import Foundation
+
+enum Secrets: String {
+  case youtubeApiKey = ""
+}
